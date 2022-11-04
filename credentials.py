7635f15379f6db6257495693fcf07fdf6af6ef41
Jason Sabeniano <35453938+Sabeniano@users.noreply.github.com>
2022-11-04 15:18:12 +0100

    updated files for final project

--- credentials.py
+++ credentials.py
@@ -1,6 +1,6 @@
 credentials = {
-    'ssid' : 'Waoo4920_83HY', # KEA_Starlink | tolga | Waoo4920_83HY
-    'password' : 'pkhn9837', # starlink_kea | iottest123 | pkhn9837
+    'ssid' : 'LTE-1857', # KEA_Starlink | LTE-1857 | Waoo4920_83HY |
+    'password' : '12345678', # starlink_kea | 12345678 | pkhn9837 |
     'ADAFRUIT_IO_URL' : b'io.adafruit.com',
     'ADAFRUIT_USERNAME' : b'Tolga12',
     'ADAFRUIT_IO_KEY' : b'aio_WpOL03uK1oX0FbcwjWdZdEdZQUK1',
--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import _thread
+#Table of context:
+#6 - 13 imported libary
+#14 - 33 Global Variables
+#33 - 37 Clear Neo ring
+#39 - 62 Start Neoring
+#63 - 95 Adafruit GPS
+
 import umqtt_robust2 as mqtt
 from machine import Pin,ADC
 from time import sleep
@@ -5,7 +13,6 @@ from machine import PWM
 import adafruit_gps_main
 import tm1637
 import neopixel
-import _thread
 
 analog_pin = ADC(Pin(34))
 analog_pin.atten(ADC.ATTN_11DB)
@@ -23,10 +30,7 @@ first = Tilt.value()
 sleep(0.1)
 second = Tilt.value()
 
-
-# Her kan i placere globale varibaler, og instanser af klasser
-# instans af Pin klassen AKA et Pin objekt
-tm = tm1637.TM1637(clk=Pin(2), dio=Pin(4))
+tm = tm1637.TM1637(clk=Pin(18), dio=Pin(19))
 
 def clear_neopixel():
     for i in range(n):
@@ -40,11 +44,11 @@ def tilt_neopixel():
         second = Tilt.value()
         global counter
         
-        print("TEST LOOP1")
+        #print("TEST LOOP1")
         if first == 1 and second == 0:
             print("TEST LYS")
             counter += 1
-            if counter >= 1:
+            if counter >= 1 and counter < 12:
                 for i in range(counter):
                     np[i] = (0, 170, 0)
                     np.write()
@@ -52,30 +56,30 @@ def tilt_neopixel():
                 for i in range(n):
                     np[i] = (170, 0, 0)
                     np.write()
-            sleep(5)
+            sleep(2)
 
 clear_neopixel()
 _thread.start_new_thread(tilt_neopixel, ())
 
 while True:
-    print("TEST LOOP2")
     try:
         # Jeres kode skal starte her
-
+        
         #batteri
         analog_val = analog_pin.read()
-        volts = (analog_val * 0.00095545)*5
+        volts = (analog_val * 0.000350545)*5
         battery_percentage = volts*100 - 320
         print("Volt:", volts, "v")
-        print("The Batter percentage is:", battery_percentage / 2, "%")
-        realBattery = battery_percentage / 2
+        print("The Batter percentage is:", battery_percentage / 3, "%")
+        realBattery = battery_percentage / 3
+        realBattery
         
-        mqtt.web_print(battery_percentage / 2, 'Tolga12/feeds/Min Feed/csv') #Vigtig for feed/dashboard
+        mqtt.web_print(battery_percentage / 3, 'Tolga12/feeds/Min Feed/csv') #Vigtig for feed/dashboard
         tm.number(int(realBattery))
         sleep(4)
 
         #GPS       
-        adafruit_gps_main.GPS()
+        adafruit_gps_main.GPS() #Husk det er den grønne ledning ú
     
            
         # Jeres kode skal slutte her
@@ -89,3 +93,4 @@ while True:
         print('Ctrl-C pressed...exiting')
         mqtt.c.disconnect()
         mqtt.sys.exit()
+
